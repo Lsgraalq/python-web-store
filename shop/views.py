9c13d65a28e9bf2536244d787c927208c0a33208
lsgraalq <ilya.merinec@gmail.com>
2024-03-14 20:02:28 +0200

    add templates

--- shop/views.py
+++ shop/views.py
@@ -1,26 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
 
 # Create your views here.
-from .models import *
-
-
 
 def product_list(request, category_slug=None):
-    category = None 
+    category = None
     categories = Category.objects.all()
-    product = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(Category, slug = category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html',
-                  {'category':category,
-                   'categories':categories,
-                   'products':products,})
+    return render(request, 
+                  'shop/product/list.html', 
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
 
-def product_detail(request,id,slug):
+def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop.product/detail.html',{'product':product})
-
-
-
-
+    return render(request, 'shop/product/detail.html', {'product': product})
